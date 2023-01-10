fe29d8a3d363bf274b1aceae35c21c48e4e0cf86
dangelysm <dangelysmarino14gmail.com>
2023-01-10 09:56:42 -0500

    encriptar

--- proyecto/js/index.js
+++ proyecto/js/index.js
@@ -3,34 +3,42 @@ function ingresar() {
     var passwordsistema = '123456789'
     var mensajeerror = 'contraseña incorrecta'
     var password = document.getElementById("password").value;
+    var nombreusser = document.getElementById("nombre").value
 
+    if (password == passwordsistema) {
+        document.getElementById("error").innerHTML = " ";
+        location.href = "opciones.html";
+        localStorage.setItem ("nombre" , nombreUsser);
 
-    if(password == passwordsistema) {
+    } else {
 
-    document.getElementById("error").innerHTML=" ";
-    location.href ="sistema.html"
-
-
-    }else{
-        
-         document.getElementById("error").innerHTML= mensajeerror;
+        document.getElementById("error").innerHTML = mensajeerror;
     }
-
+function muestra (){
+    document.getElementById("saludo").innerHTML+=localStorage.getItem("nombre");
+}
+muestra();
+}
+function redireccion1() {
+    location.href = "encriptar.html"
 
 }
+function redireccion2() {
+    location.href = "desencriptar.html"
+}
 
-function encriptar(){
-var mensaje = document.getElementById("mensaje").value;
-var mensajeencriptado = btoa(mensaje);
-document.getElementById("msjencript").innerHTML = mensajeencriptado;
+function encriptar() {
+    var mensaje = document.getElementById("mensaje").value;
+    var mensajeencriptado = btoa(mensaje);
+    document.getElementById("msjencript").innerHTML = mensajeencriptado;
 
 }
-function desencriptar(){
+function desencriptar() {
     var mensajecodigo = document.getElementById("msgcodigo").value;
     var mensajedesencriptado = atob(mensajecodigo);
 
     document.getElementById("msjreal").innerHTML = mensajedesencriptado;
 }
-    
+
 
 
