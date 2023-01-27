85689ecb5eca4f2014d4d11f1ccc7e71a63928c4
DangelYssss3 <122289445+dangelyM@users.noreply.github.com>
2023-01-27 11:31:07 -0500

    Add files via upload

---
+++ dangelys/unidad 3/array_busqueda/js/main.js
@@ -0,0 +1,30 @@
+// aplicando lo aprendido
+//  ========== indexof ==========
+
+//let array = [2,9,2];
+
+
+//console.log(array.indexOf(9)); //devuelve la posicion 
+
+//cuando se encuentra el valor se devuelve -1
+//onsole.log(array.indexOf(0));
+
+// aplicando lo aprendido2
+//  ========== lastindexof ==========
+
+let array = [2,5,9,2];
+
+//console.log(array.lastIndexOf(2,2));
+//console.log(array.lastIndexOf(2,.3));
+//console.log(array.lastIndexOf(2,1));
+
+// aplicando lo aprendido3
+//  ========== includes ==========
+console.log([1,2,3].includes(2));
+console.log([1,2,3].includes(4));
+console.log([1,2,3].includes(3,3));
+console.log([1,2,3].includes(3,-1));
+console.log([1,2,NaN].includes(NaN));
+
+
+
